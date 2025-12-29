073e69e566abe472d2f30a15ac5d989f08901dad
FredrikM <fredric.malmstrom@gmail.com>
2025-12-29 21:30:01 +0100

    Initial structure frontend

--- src/frontend/domyra-app/src/app/app.routes.ts
+++ src/frontend/domyra-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Devices } from './pages/devices/devices';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'devices', component: Devices },
+];
